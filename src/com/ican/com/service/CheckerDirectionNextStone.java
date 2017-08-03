5ad062c30c4bf9a4d0d2d6d0bd47d4a09dfb3ccb
kim <kim@kim-PC>
2017-08-04 00:21:50 +0900

    modify

--- src/com/ican/com/service/CheckerDirectionNextStone.java
+++ src/com/ican/com/service/CheckerDirectionNextStone.java
@@ -1,17 +1,18 @@
 package com.ican.com.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ican.com.bean.DirectionNorthWest;
 import com.ican.com.bean.OthelloBean;
 
 public class CheckerDirectionNextStone {
-	public DirectionNorthWest northWestNextStone(OthelloBean inputBean, List<OthelloBean> beanList){
+	public List<DirectionNorthWest> northWestNextStone(OthelloBean inputBean, List<OthelloBean> beanList){
+		List<DirectionNorthWest> northWestList= new ArrayList<DirectionNorthWest>();
 		DirectionNorthWest northWest = new DirectionNorthWest(inputBean.getPositionX(), inputBean.getPositionY());
-		northWest.setCountX(processCounter(beanList));
-		northWest.setCountY(processCounter(beanList));
+		
 		System.out.println("계산된 x 좌표 : "+northWest.getProcessX()+", 계산 된 y 좌표 : "+northWest.getProcessY());
-		return northWest;
+		return northWestList;
 	}
 	
 	public int processCounter(List<OthelloBean> beanList){
